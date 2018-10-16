330da11acb44bde1cfc035e50492edf62f615297
Hui Zhou <hzhou@hzsolutions.net>
2018-10-15 20:11:02 -0500

    new file:   ../templates/frame.c 	new file:   ../templates/frame.h

---
+++ templates/frame.c
@@ -0,0 +1,10 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2018 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+$call @rationale
+
+DUMP_STUB global_init
+DUMP_STUB meta_init
